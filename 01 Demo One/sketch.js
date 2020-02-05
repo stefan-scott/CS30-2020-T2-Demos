596d2f5ce70f810ff8fe4e809a2a0594138b2c5b
stefan-scott <scottst@spsd.sk.ca>
2020-02-05 09:55:23 -0600

    demo sketch one

--- 01 Demo One/sketch.js
+++ 01 Demo One/sketch.js
@@ -5,11 +5,25 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let r = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  print(frameCount);
+  background(0);
+  fill(r,0,0);
+  stroke(255);
+  ellipse(mouseX,mouseY, 40, 40);
+}
+
+function mousePressed(){
+  if (r === 0){
+    r = 255;
+  }
+  else{
+    r = 0;
+  } 
 }
