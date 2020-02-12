574d854666e4d8315ed660967ed83ac67b465e4c
stefan-scott <scottst@spsd.sk.ca>
2020-02-12 13:56:50 -0600

    setup code for partner challenge

---
+++ 04 Partner State Project/sketch.js
@@ -0,0 +1,20 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+let x = 0;
+let y = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  rect(x, y, 0,0);
+  x += 1;
+  y += 1;
+
+}
