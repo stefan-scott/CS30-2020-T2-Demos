418b0b8a64859d647d2888541a733ff6397ce23c
stefan-scott <scottst@spsd.sk.ca>
2020-02-26 13:50:47 -0600

    image demo complete

--- 06 Image Demo/sketch.js
+++ 06 Image Demo/sketch.js
@@ -2,18 +2,23 @@
 
 let lionL, lionR;
 let movingLeft = true;
+let pinImages = [];
+let pinFrame = 0;
 
-function preload(){
+function preload() {
   lionL = loadImage("assets/lion-left.png");
   lionR = loadImage("assets/lion-right.png");
+  for (let i = 0; i < 9; i++) {
+    pinImages.push(loadImage("assets/pin-0" + i + ".png")); //0
+  }
 }
 
-function determineDirection(){
-  if(pmouseX < mouseX){
+function determineDirection() {
+  if (pmouseX < mouseX) {
     //which way? Moving right
     movingLeft = false;
   }
-  else if (pmouseX > mouseX){
+  else if (pmouseX > mouseX) {
     //moving left
     movingLeft = true;
   }
@@ -24,6 +29,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   imageMode(CENTER);
   noCursor();
+  frameRate(240);
 }
 
 function draw() {
@@ -31,13 +37,24 @@ function draw() {
   background(220);
   determineDirection();
 
-  
-  if(movingLeft){
+
+  if (movingLeft) {
     image(lionL, mouseX, mouseY, 130, 90);
   }
-  else{
+  else {
     image(lionR, mouseX, mouseY, 130, 90);
   }
+
+  //Code for the PinWheels
   
+  image(pinImages[pinFrame], width/2, height/2);
+
+  let speedChange = map(mouseX, 0, width,1,4);
+  if(frameCount % int(speedChange) === 0){
+    pinFrame ++;
+    if (pinFrame > 8) pinFrame = 0;
+  }
+
+
 
 }
