966681655044a404db3b2bf77e56f69b3f17a5c3
stefan-scott <scottst@spsd.sk.ca>
2020-03-05 11:47:15 -0600

    Update sketch.js

--- 10 Perlin Noise Demo/sketch.js
+++ 10 Perlin Noise Demo/sketch.js
@@ -3,6 +3,9 @@
 let x, y, size;
 const MAX_CIRCLE_SIZE = 200;
 
+let sizeTime = 10;
+let dT = 0.02;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   x = width/2;
@@ -19,8 +22,14 @@ function setSize(){
   size = random(10, MAX_CIRCLE_SIZE);
 }
 
+function setSizePerlin(){
+  size = noise(sizeTime) * MAX_CIRCLE_SIZE;
+  sizeTime += dT;
+}
+
 function drawCircle(){
-  setSize();
+  setSizePerlin();
+  print(size);
   ellipseMode(CENTER);
   ellipse(x,y,size,size);
 }
