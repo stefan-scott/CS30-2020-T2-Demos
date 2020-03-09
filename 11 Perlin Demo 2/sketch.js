85a5907b7c9a74957c9bb7a1ae3576852b819d86
stefan-scott <scottst@spsd.sk.ca>
2020-03-09 13:27:30 -0600

    completed

--- 11 Perlin Demo 2/sketch.js
+++ 11 Perlin Demo 2/sketch.js
@@ -1,19 +1,29 @@
 // Perlin Noise Demo Two
 
+let redTime = 10;
+let redSpeed = 0.04;
+let offsetAmount = 0.02;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   ellipseMode(CENTER);
+  // drawCircles();
 }
 
 function drawCircles(){
   for(let x = 0; x < width; x += 25){
     for(let y = 0; y < height; y += 25){
+      let r = map(noise(redTime),0,1,0,255);
+      fill(r, 0, 0);
       ellipse(x,y,25,25);
+      redTime += redSpeed;
     }
   }
 }
 
 function draw() {
+  redTime = 10 + offsetAmount;
   background(220);
   drawCircles();
+  offsetAmount -= 0.02;
 }
