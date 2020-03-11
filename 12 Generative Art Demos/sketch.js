47497b5832525fb345ecb81c9afded19b681d6cc
stefan-scott <scottst@spsd.sk.ca>
2020-03-11 14:10:04 -0600

    Started Cubic Disarray

--- 12 Generative Art Demos/sketch.js
+++ 12 Generative Art Demos/sketch.js
@@ -1,12 +1,27 @@
 //Diagonal Line Art 
 
+let gridSpacing = 20;
+const PADDING = 60;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
 }
 
-
+function drawGrid(){
+  for (let x = gridSpacing/2 + PADDING; x < width - PADDING; x += gridSpacing){
+    for (let y = gridSpacing/2 + PADDING; y < height - PADDING; y+= gridSpacing){
+      rect(x,y,gridSpacing,gridSpacing);
+    }
+  }  
+}
 function draw() {
   background(220);
+  border();
+  drawGrid();
 }
-
+function border(){
+  strokeWeight(15);
+  rect(width/2,height/2,width,height);
+  strokeWeight(2);
+}
