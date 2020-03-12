4cdefcbec1ca890239abb5e0a24c0c8c95722ee1
stefan-scott <scottst@spsd.sk.ca>
2020-03-12 13:26:46 -0600

    nearly compelte

---
+++ 12 Generative Art Demos/Diagonal Lines.js
@@ -0,0 +1,45 @@
+//Diagonal Line Art 
+
+let GRID_SIZE = 20;
+let seed;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  seed = random(1000);
+}
+
+function diagonal(x, y, s, dir){
+  //dir:  0 → Ascending   1 or anything else  → Descending
+  if(dir === 0){ //Ascending
+    line(x - s/2, y + s/2, x + s/2, y - s/2);
+  }
+  else if (dir === 1){ //Descending
+    line (x - s/2, y - s/2, x + s/2, y + s/2);
+  }
+}
+
+function draw() {
+  GRID_SIZE = map(mouseX,0,width,3, 60);
+  randomSeed(seed);
+  background(220);
+  border();
+  createTiles();
+}
+
+function createTiles(){
+  for (let x = GRID_SIZE/2; x < width ; x += GRID_SIZE){
+    for (let y = GRID_SIZE/2; y < height; y += GRID_SIZE){
+      diagonal(x,y,GRID_SIZE,int(random(2)));
+    }
+  }
+}
+
+function border(){
+  strokeWeight(15);
+  rect(0,0,width,height);
+  strokeWeight(2);
+}
+
+
+
+
--- 12 Generative Art Demos/sketch.js
+++ 12 Generative Art Demos/sketch.js
@@ -6,17 +6,25 @@ const PADDING = 60;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
+  noFill();
 }
 
 function drawGrid(){
   for (let x = gridSpacing/2 + PADDING; x < width - PADDING; x += gridSpacing){
     for (let y = gridSpacing/2 + PADDING; y < height - PADDING; y+= gridSpacing){
-      rect(x,y,gridSpacing,gridSpacing);
+      let amplitude = map(y,gridSpacing/2+PADDING, height-PADDING, 0, 45);
+      push();
+      translate(x,y);
+      rotate(radians(random(-amplitude,amplitude)));
+      rect(0,0,gridSpacing,gridSpacing);
+      pop();
+
     }
   }  
 }
 function draw() {
   background(220);
+  randomSeed(10);
   border();
   drawGrid();
 }
