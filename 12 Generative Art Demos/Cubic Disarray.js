71a3a98872a603387caa19ece7eff3e3ea5b0dbf
stefan-scott <scottst@spsd.sk.ca>
2020-03-13 11:07:11 -0600

    updated

---
+++ 12 Generative Art Demos/Cubic Disarray.js
@@ -0,0 +1,38 @@
+//Diagonal Line Art 
+
+let gridSpacing = 20;
+const PADDING = 60;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
+  noFill();
+}
+
+function drawGrid(){
+  for (let x = gridSpacing/2 + PADDING; x < width - PADDING; x += gridSpacing){
+    for (let y = gridSpacing/2 + PADDING; y < height - PADDING; y+= gridSpacing){
+      let amplitude = map(y,gridSpacing/2+PADDING, height-PADDING, 0, 45);
+      push();
+      translate(x,y);
+      rotate(radians(random(-amplitude,amplitude)));
+      translate(random(-amplitude/5, amplitude/5), 0);
+      translate(0, random(-amplitude/5, amplitude/2));
+
+      rect(0,0,gridSpacing,gridSpacing);
+      pop();
+
+    }
+  }  
+}
+function draw() {
+  background(220);
+  randomSeed(10);
+  border();
+  drawGrid();
+}
+function border(){
+  strokeWeight(15);
+  rect(width/2,height/2,width,height);
+  strokeWeight(2);
+}
--- 12 Generative Art Demos/sketch.js
+++ 12 Generative Art Demos/sketch.js
@@ -1,6 +1,7 @@
 //Diagonal Line Art 
 const RECT_WIDTH = 10;
 const RECT_HEIGHT = 50;
+let colors = []; //fill with HEX codes as Strings.
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -10,7 +11,25 @@ function draw() {
   background(220);
   randomSeed(0);
   drawRowRGB(height*0.2);
+  drawRowHSB(height*0.5);
 }
+
+function drawRowCustom(yPos){
+  colorMode(RGB,255);
+  for(let x = 0; x < width; x += RECT_WIDTH){
+    fill();
+    rect(x, yPos, RECT_WIDTH, RECT_HEIGHT);
+  }
+}
+
+function drawRowHSB(yPos){
+  colorMode(HSB, 360);
+  for(let x = 0; x < width; x += RECT_WIDTH){
+    fill(x/3 % 360, 300, map(mouseY,0,height,0,300));
+    rect(x, yPos, RECT_WIDTH, RECT_HEIGHT);
+  }
+}
+
 
 function drawRowRGB(yPos){
   colorMode(RGB,255);
